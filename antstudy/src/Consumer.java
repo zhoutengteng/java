ecf21c0d535de865c7cf58e8d2a92a1fbe104eb2
zhoutengteng <865998332@qq.com>
2015-08-19 00:55:57 +0800

    利用ant编译出jar文件  使用java -jar 文件.jar 然后就可以运行了

---
+++ antstudy/src/Consumer.java
@@ -0,0 +1,28 @@
+
+import java.lang.Thread;
+
+public class Consumer implements Runnable {
+    private SyncStack theStack;
+    private int num;
+    private static int counter = 1;
+
+    public Consumer(SyncStack s) {
+        theStack = s;
+        num = counter++;
+    }
+
+    public void run() {
+        char c;
+        for (int i = 0; i < 200; i++) {
+            c = theStack.pop();
+//            System.out.print(num+"POP....................  ");
+            System.out.println("Consumer"+num+": " + c);
+            try {
+                 Thread.sleep((int)Math.random()*300);   
+             }  catch (InterruptedException e) {
+
+             }
+        }
+    }
+
+}
