7b938889d6affdc3105ca171880b723e0ddb82e6
zhoutengteng <865998332@qq.com>
2015-08-21 00:24:32 +0800

    包测试

---
+++ testPackage/Apple.java
@@ -0,0 +1,8 @@
+package testPackage;
+
+public class Apple {
+    private int a;
+    public Apple() {
+        a = 0;
+    }
+}
---
+++ testPackage/teng/Banana.java
@@ -0,0 +1,14 @@
+package testPackage.teng;
+import testPackage.Apple;
+
+
+public class Banana {
+    private int a;
+    public Banana() {
+        a = 0;
+    }
+    public static void main(String []argv) {
+        Apple ss = new Apple();
+
+    }
+}
